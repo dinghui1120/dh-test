ea606e2c9677c416a854d04d79b5dfb6f6e69502
“dingui” <dh243542757@gmail.com>
2022-11-20 15:47:13 +0800

    init

---
+++ src/test/java/com/dh/test/DhTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
