93c0b8deb9f72935841a01342406614bf1d969a6
dinghui <dh243542757@gmail.com>
2024-04-12 00:49:25 +0800

    spi

---
+++ src/main/java/com/dh/spi/spring/MyAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.dh.spi.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyAutoConfiguration
+ * @author: dh
+ * @date: 2024年03月24日
+ **/
+@Configuration
+@ComponentScan(basePackages = "com.example")
+public class MyAutoConfiguration {
+
+
+
+}
